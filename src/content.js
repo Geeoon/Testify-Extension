10885edd6e98ee78f5739f796c811b790bb84d40
Geeoon <geeoon13@gmail.com>
2022-02-23 10:03:44 -0600

    Finished program; might add more settings

--- src/content.js
+++ src/content.js
@@ -1,11 +1,22 @@
 document.body.onload = addElement;
 
+var maxOpacity = .05;
 var wrapperDiv = document.createElement("div");
 wrapperDiv.setAttribute('id', 'testify-extension-wrapper');
-wrapperDiv.innerHTML = "TEST";
+wrapperDiv.style.opacity = 0.0;
 
 var mouseX;
 var mouseY;
+
+var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+if (!location.ancestorOrigins.contains(extensionOrigin)) {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('id', 'testify-extension-iframe');
+    // Must be declared at web_accessible_resources in manifest.json
+    iframe.src = chrome.runtime.getURL('frame.html');
+
+    wrapperDiv.appendChild(iframe);
+}
 
 function addElement() {
     const bodyTag = document.getElementsByTagName("BODY")[0];
@@ -17,14 +28,20 @@ document.onkeydown = changePosition;
 function changePosition(event) {
     event = event || window.event;
     if (event.keyCode == 115) {  // F4
-        wrapperDiv.style.left = 500;
-        alert(wrapperDiv.style.left);
-        wrapperDiv.style.top = 500;
+        wrapperDiv.style.left = String(mouseX).concat("px");
+        wrapperDiv.style.top = String(mouseY).concat("px");
+    } else if (event.keyCode == 220) {  // Alt
+        if (wrapperDiv.style.zIndex != -9999999) {
+            wrapperDiv.style.zIndex = -9999999;
+            wrapperDiv.style.opacity = 0;
+        } else {
+            wrapperDiv.style.zIndex = 9999999;
+            wrapperDiv.style.opacity = maxOpacity;
+        }
     }
 }
-/*
+
 onmousemove = function(e) {
     mouseX = e.clientX;
-    mouseX = e.clientY;
+    mouseY = e.clientY;
 }
-*/
